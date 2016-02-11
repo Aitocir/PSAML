4f7d677a6db4b26a0dec73cc6dfd184c88c90af2
Aitocir <tommy@halegends.com>
2016-02-11 11:16:47 -0700

    derpy code is still derpy but more talky; also added CSV iris dataset

--- iris_analysis.py
+++ iris_analysis.py
@@ -7,23 +7,33 @@ from pyspark.ml import Pipeline
 from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.ml.feature import VectorIndexer
 from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.feature import VectorAssembler
 
 # init
 sc = SparkContext("local", "Simple App")
 sqlContext = SQLContext(sc)
 
 # Load the data stored in LIBSVM format as a DataFrame.
-data = sqlContext.read.format("libsvm").load("data/mllib/sample_iris_data.txt")
+# data = sqlContext.read.format("libsvm").load("data/mllib/sample_iris_data.txt")
+# data = sqlContext.read.format("csv").load("data/mllib/iris.csv")
+
+# header=false so the columns aren't named after the first row values
+# inferSchema=true so that data is read in as correct datatype, not just strings
+data = sqlContext.read.load('data/mllib/iris.csv', format='com.databricks.spark.csv', header='false', inferSchema='true')
 print("DATA:\n")
 print(data)
 
+# now we create a vector of the input columns so they can be one column
+ignore = ["C4"]  # ignore the output column
+assembler = VectorAssembler(inputCols=[x for x in data.columns if x not in ignore], outputCol='features')
+data = assembler.transform(data)  # now we've added a 6th column named 'features'
+
 # Automatically identify categorical features, and index them.
 # We specify maxCategories so features with > 4 distinct values are treated as continuous.
-featureIndexer =\
-    VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
 
 # Read in data for sensitivity analysis
-testData = sqlContext.read.format("libsvm").load("data/mllib/iris_analysis_column4.txt")
+testData = sqlContext.read.format("csv").load("data/mllib/iris_test_data.csv")
 
 # Train a DecisionTree model.
 dt = DecisionTreeRegressor(featuresCol="indexedFeatures")
