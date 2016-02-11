b97151a198047c6ceb724342c754b24baf0368ab
Aitocir <tommy@halegends.com>
2016-02-11 12:01:03 -0700

    clearer code from help sesh with Marissa

--- iris_analysis.py
+++ iris_analysis.py
@@ -5,9 +5,8 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.ml import Pipeline
 from pyspark.ml.regression import DecisionTreeRegressor
-from pyspark.ml.feature import VectorIndexer
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import VectorAssembler, StringIndexer, VectorIndexer
 
 # init
 sc = SparkContext("local", "Simple App")
@@ -26,20 +25,21 @@ print(data)
 # now we create a vector of the input columns so they can be one column
 ignore = ["C4"]  # ignore the output column
 assembler = VectorAssembler(inputCols=[x for x in data.columns if x not in ignore], outputCol='features')
-data = assembler.transform(data)  # now we've added a 6th column named 'features'
+#  data = assembler.transform(data)  # now we've added a 6th column named 'features'
 
 # Automatically identify categorical features, and index them.
 # We specify maxCategories so features with > 4 distinct values are treated as continuous.
-featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+featureIndexer = VectorIndexer(inputCol="features", outputCol="indexed")
+classIndexer = StringIndexer(inputCol="C4", outputCol="label")
 
 # Read in data for sensitivity analysis
-testData = sqlContext.read.format("csv").load("data/mllib/iris_test_data.csv")
+testData = sqlContext.read.load('data/mllib/iris_test_data.csv', format='com.databricks.spark.csv', header='false', inferSchema='true')
 
 # Train a DecisionTree model.
-dt = DecisionTreeRegressor(featuresCol="indexedFeatures")
+dt = DecisionTreeRegressor(featuresCol="indexed", labelCol="label")
 
 # Chain indexer and tree in a Pipeline
-pipeline = Pipeline(stages=[featureIndexer, dt])
+pipeline = Pipeline(stages=[featureIndexer, classIndexer, dt])
 
 # Train model.  This also runs the indexer.
 model = pipeline.fit(data)
