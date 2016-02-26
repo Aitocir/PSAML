acb2d6ed27b60ef9f12cc941c8b435b1ee4c04e4
Sean Lane <sean@seanlane.net>
2016-02-25 22:24:20 -0700

    Update style to follow Spark standards, begin handling Issue #3, get test parameters set

--- psaml/psaml.py
+++ psaml/psaml.py
@@ -9,78 +9,82 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.feature import VectorAssembler, StringIndexer, VectorIndexer
 
 
-def doContinuousInputAnalysis(sc, model, expSensitivity, ctrlSensitivity, dataInfo):
+def do_continuous_input_analysis(sc, model, exp_sensitivity, ctrl_sensitivity, data_info):
+    # -1) create SQLContext
+    sql_context = SQLContext(sc)
 
-   # -1) create SQLContext
-   sqlContext = SQLContext(sc)
+    # ##########################################################################################################
+    #
+    # 0) Verify input
+    #
+    #  assert expSensitivity > 0 (int)
+    #  assert ctrlSensitivity > 0 (int)
+    #  assert dataInfo (DataFrame of the following format, one row for each column in the data model works on):
+    #
+    #                                         DataFrame of Data columns
+    #                     _________________________________________________________________
+    # Column purpose     | colName   | minValue  | maxValue  | shouldAnalyze   | isClass   |
+    #                    |-----------|-----------|-----------|-----------------|-----------|
+    # Column type        | string    | numeral   | numeral   | boolean         | boolean   |
+    #                    |-----------|-----------|-----------|-----------------|-----------|
+    # Example record     | "petalW"  | 4.3       | 7.9       | true            | false     |
+    #                    |___________|___________|___________|_________________|___________|
+    #
+    try:
+        assert (exp_sensitivity > 0), "Experiment Sensitivity must be a positive integer"
+        assert (ctrl_sensitivity > 0), "Control Variable Sensitivity must be a positive integer"
+    except AssertionError as e:
+        raise ValueError(e.args)
 
-   # ##########################################################################################################
-   #
-   # 0) Verify input
-   #
-   #  assert expSensitvity > 0 (int)
-   #  assert ctrlSensitivity > 0 (int)
-   #  assert dataInfo (DataFrame of the following format, one row for each column in the data model works on):
-   #
-   #                                         DataFrame of Data columns
-   #                     _________________________________________________________________
-   # Column purpose     | colName   | minValue  | maxValue  | shouldAnalyze   | isClass   |
-   #                    |-----------|-----------|-----------|-----------------|-----------|
-   # Column type        | string    | numeral   | numeral   | boolean         | boolean   |
-   #                    |-----------|-----------|-----------|-----------------|-----------|
-   # Example record     | "petalW"  | 4.3       | 7.9       | true            | false     |
-   #                    |___________|___________|___________|_________________|___________|
-   #
+    # ##########################################################################################################
+    #
+    # 1) Generate test data
+    #
+    #  TODO: generate test data based on a collection of mins, maxs
+    #
+    #  testData = a new dataframe, whose column names = all values from the colName col from dataInfo
+    #  for ( x : 0 ... ctrlSensitivity ), inclusive
+    #     foreach ( varCol : varCol.shouldAnalyze == true )
+    #        for ( y : 0 ... expSensitivity ), inclusive
+    #           load record into testData
+    #           #  set all values to minValue + ((maxValue - minValue) * (x / ctrlSensitivity))
+    #           #  manually set varCol to minValue + ((maxValue - minValue) * (y / ctrlSensitivity))
+    #           #  value loaded into the class column does NOT matter
+    #
 
+    # ##########################################################################################################
+    #
+    # 2) Make predictions.
+    #
+    # predictions = model.transform(testData)  #  but, we're not passing in dataInfo yet, so we'll treat
+    # dataInfo like already done testData
+    predictions = model.transform(data_info)
 
-   # ##########################################################################################################
-   #
-   # 1) Generate test data
-   #
-   #  TODO: generate test data based on a collection of mins, maxs
-   #
-   #  testData = a new dataframe, whose column names = all values from the colName col from dataInfo
-   #  for ( x : 0 ... ctrlSensitivity ), inclusive
-   #     foreach ( varCol : varCol.shouldAnalyze == true )
-   #        for ( y : 0 ... expSensitivity ), inclusive
-   #           load record into testData
-   #           #  set all values to minValue + ((maxValue - minValue) * (x / ctrlSensitivity))
-   #           #  manually set varCol to minValue + ((maxValue - minValue) * (y / ctrlSensitivity))
-   #           #  value loaded into the class column does NOT matter
-   #
+    # ##########################################################################################################
+    #
+    # 3) Transform predictions into output DataFrame
+    #
+    #  Output DataFrame should use the following format:
+    #
+    #                                            dataframe name
+    #                     _______________________________________________________________
+    # Column purpose     | prediction   | varColName   | expVariance   | ctrlVariance    |
+    #                    |--------------|--------------|---------------|-----------------|
+    # Column types       | <classType>  | string       | num (0.0-1.0) | num (0.0-1.0)   |
+    #                    |--------------|--------------|---------------|-----------------|
+    # Example record     | "iris-setosa"| "PetalW"     | 0.7           | 0.2             |
+    #                    |______________|______________|_______________|_________________|
+    #
+    # In the above example record, we get "iris-setosa" as a prediction when holding "PetalW" at 70% of potential
+    # value, and everything else at 20%
+    #
+    #     varianceData = new DataFrame after above format
+    #     for ( x : 0 ... ctrlSensitivity ), inclusive
+    #        foreach ( varCol : varCol.shouldAnalyze == true )
+    #           for ( y : 0 ... expSensitivity ), inclusive
+    #              translate row from predictions[n] to  varianceData[n]
+    #              #  they will end up being the same size
+    #
 
-   # ##########################################################################################################
-   #
-   # 2) Make predictions.
-   #
-   #  predictions = model.transform(testData)  #  but, we're not passing in dataInfo yet, so we'll treat dataInfo like alraedy done testData
-   predictions = model.transform(dataInfo)
-
-   # ##########################################################################################################
-   #
-   # 3) Transform predictions into output DataFrame
-   #
-   #  Output DataFrame should use the following format:
-   #
-   #                                            dataframe name
-   #                     _______________________________________________________________
-   # Column purpose     | prediction   | varColName   | expVariance   | ctrlVariance    |
-   #                    |--------------|--------------|---------------|-----------------|
-   # Column types       | <classType>  | string       | num (0.0-1.0) | num (0.0-1.0)   |
-   #                    |--------------|--------------|---------------|-----------------|
-   # Example record     | "iris-setosa"| "PetalW"     | 0.7           | 0.2             |
-   #                    |______________|______________|_______________|_________________|
-   #
-   #  # in the above example record, we get "iris-setosa" as a prediction when holding "PetalW" at 70% of potential value, and everything else at 20%
-   #
-   #     varianceData = new DataFrame after above format
-   #     for ( x : 0 ... ctrlSensitivity ), inclusive
-   #        foreach ( varCol : varCol.shouldAnalyze == true )
-   #           for ( y : 0 ... expSensitivity ), inclusive
-   #              translate row from predictions[n] to  varianceData[n]
-   #              #  they will end up being the same size
-   #
-   
-   # return varianceData  # but for now, just return predictions so the code actually interprets
-   return predictions;
-   
+    # return varianceData  # but for now, just return predictions so the code actually interprets
+    return predictions
--- tests/test_psaml.py
+++ tests/test_psaml.py
@@ -1,6 +1,9 @@
 #
 # Iris Analysis
 #
+import os
+import sys
+
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.ml import Pipeline
@@ -10,13 +13,19 @@ from pyspark.ml.feature import VectorAssembler, StringIndexer, VectorIndexer
 
 # init
 sc = SparkContext('local', 'Test_PSAML')
-sc.addPyFile("/Users/aitocir/Documents/cs401r/ThinkBig/PSAML/psaml/psaml.py")
-from psaml import *
-sqlContext = SQLContext(sc)
+
+# Get parent directory of the tests directory
+parent_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.join(parent_dir, 'psaml'))
+import psaml
+
+sc.addPyFile(os.path.join(parent_dir, 'psaml/psaml.py'))
+
+sql_context = SQLContext(sc)
 
 # header=false so the columns aren't named after the first row values
-# inferSchema=true so that data is read in as correct datatype, not just strings
-data = sqlContext.read.load('data/iris.csv', format='com.databricks.spark.csv', header='false', inferSchema='true')
+# inferSchema=true so that data is read in as correct data type, not just strings
+data = sql_context.read.load('resources/iris.csv', format='com.databricks.spark.csv', header='false', inferSchema='true')
 
 # now we create a vector of the input columns so they can be one column
 ignore = ['C4']  # ignore the output column
@@ -24,23 +33,26 @@ assembler = VectorAssembler(inputCols=[x for x in data.columns if x not in ignor
 
 # Automatically identify categorical features, and index them.
 # We specify maxCategories so features with > 4 distinct values are treated as continuous.
-featureIndexer = VectorIndexer(inputCol="features", outputCol="indexed")
-classIndexer = StringIndexer(inputCol="C4", outputCol="label")
+feature_indexer = VectorIndexer(inputCol="features", outputCol="indexed")
+class_indexer = StringIndexer(inputCol="C4", outputCol="label")
 
 # Read in data for sensitivity analysis
-testData = sqlContext.read.load('data/iris_test_data.csv', format='com.databricks.spark.csv', header='false', inferSchema='true')
+test_data = sql_context.read.load('resources/iris_test_data.csv',
+                                  format='com.databricks.spark.csv',
+                                  header='false',
+                                  inferSchema='true')
 
 # Train a DecisionTree model.
 dt = DecisionTreeRegressor(featuresCol="indexed", labelCol="label")
 
 # Chain indexer and tree in a Pipeline
-pipeline = Pipeline(stages=[assembler, featureIndexer, classIndexer, dt])
+pipeline = Pipeline(stages=[assembler, feature_indexer, class_indexer, dt])
 
 # Train model.  This also runs the indexer.
 model = pipeline.fit(data)
 
 # Make predictions.
-predictions = doContinuousInputAnalysis(sc, model, 1, 1, testData)
+predictions = psaml.do_continuous_input_analysis(sc, model, 1, 1, test_data)
 
 # Select example rows to display.
 predictions.select("prediction", "label", "features").show()  # opt param: number of records to show
